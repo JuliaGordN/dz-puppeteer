454302c03ecfeecaaceb459125ad43465753e30e
JuliaGordN <ms.vendystar@gmail.com>
2022-01-20 13:03:58 +0200

    eslint fix

--- testpdf.js
+++ testpdf.js
@@ -1,22 +1,21 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
-    try {
-        const page = await browser.newPage();
-        await page.goto('https://pptr.dev/', {
-        waitUntil: 'networkidle0',
-    })
-        await page.waitForSelector('input');
-        await page.keyboard.type('pdf');
-        await page.keyboard.press('Enter');
+  const browser = await puppeteer.launch();
+  try {
+    const page = await browser.newPage();
+    await page.goto('https://pptr.dev/', {
+      waitUntil: 'networkidle0',
+    });
+    await page.waitForSelector('input');
+    await page.keyboard.type('pdf');
+    await page.keyboard.press('Enter');
 
-        await page.pdf({path: 'page.pdf'});
+    await page.pdf({ path: 'page.pdf' });
 
-        await browser.close();
-
-}   catch (error) {
-    console.log("Ops... You have error in code");
+    await browser.close();
+  } catch (error) {
+    console.log('Ops... You have error in code');
     await browser.close();
   }
 })();
