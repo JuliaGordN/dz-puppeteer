cd4bd5d794890c2019300c6425b4a3d48605e09e
JuliaGordN <ms.vendystar@gmail.com>
2022-01-20 04:03:37 +0200

    dz

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# dz
- 
+# DZ puppeteer
+ <img src="https://c.tenor.com/IOMLFKCa9ucAAAAC/nope-monkey.gif " width="833" height="466" />
+
+
+1. Create a script that should:
+ Open in new tab https://pptr.dev/
+ Enter the word pdf in the search bar
+ Press Enter
+ Take a screenshot of the page that will give out as a result
+ Save screenshot as pdf
+2. Create a script that should:
+ Open https://pptr.dev/
+ Change navigation bar color to #1F54C0
+ Save page result to png
+3. Created pdf and png files need to be committed along with scripts
---
+++ testbackground.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({ 
+        headless: false,
+        slowMo: 200,
+    });
+    try {
+    const page = await browser.newPage();
+    await page.goto('https://pptr.dev/', { 
+        waitUntil: 'networkidle0' 
+    });
+    const toolbar = await page.$('body > toolbar-component');
+    const sidebar = await page.$('body > sidebar-component');
+
+    await toolbar.evaluate((el) => {
+        const toolbarStyle = el;
+        toolbarStyle.style.background = '#1F54C0';
+        return toolbarStyle.style.background;
+    });
+
+    await sidebar.evaluate((el) => {
+        const sidebarStyle = el;
+        sidebarStyle.style.background = '#1F54C0';
+        return sidebarStyle.style.background;
+    });
+
+    await page.screenshot({ path: 'img-with-background.png' });
+    await browser.close();
+
+  } catch (error) {
+    console.log("Ops... You have error in code");
+    await browser.close();
+  }
+})();
